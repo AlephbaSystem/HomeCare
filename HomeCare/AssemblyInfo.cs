98efa3ed6e177037e30e696f154695dddfab640c
soshika <soshika1010@gmail.com>
2021-09-14 12:10:53 +0430

    all font changed to iran-sans also some ui bug such as button background color fix

--- HomeCare/AssemblyInfo.cs
+++ HomeCare/AssemblyInfo.cs
@@ -2,6 +2,4 @@
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("irsans.ttf", Alias = "irsans")]
-[assembly: ExportFont("irsansb.ttf", Alias = "irsansb")]
-[assembly: ExportFont("Dirooz.ttf", Alias = "dirooz")]
+[assembly: ExportFont("IRANSansWeb.ttf", Alias = "ISans")]
