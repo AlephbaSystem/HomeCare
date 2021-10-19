a75bfb10e5e558da2caa7681fa5d9098730eb792
soshika <soshika1010@gmail.com>
2021-10-19 14:01:14 +0330

    add functionality to system

---
+++ HomeCare/ViewModels/AddDeviceViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HomeCare.ViewModels
+{
+    public class AddDeviceViewModel: INotifyPropertyChanged
+    {
+        public AddDeviceViewModel()
+        {
+        }
+
+        private string deviceName;
+        private string phone;
+        private bool isBusy;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public string DeviceName
+        {
+            get { return deviceName; }
+            set
+            {
+                deviceName = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+}
---
+++ HomeCare/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HomeCare.ViewModels
+{
+    public class MainPageViewModel: INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            StatusDeviceCommand = new Command(GetStatus);
+        }
+
+        private void GetStatus(object obj)
+        {
+            Task.Delay(100);
+            
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand StatusDeviceCommand { get; }
+
+    }
+}
