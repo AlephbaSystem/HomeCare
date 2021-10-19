2e9f2c958c5ce3e3a30bb80a44f83e1c849077e7
soshika <soshika1010@gmail.com>
2021-10-19 16:43:21 +0330

    lock, unlock, half lock is up & running

--- HomeCare/ViewModels/MainPageViewModel.cs
+++ HomeCare/ViewModels/MainPageViewModel.cs
@@ -12,16 +12,45 @@ namespace HomeCare.ViewModels
         public MainPageViewModel()
         {
             StatusDeviceCommand = new Command(GetStatus);
+            LockDeviceCommand = new Command(SetToLockDevice);
+            UnLockDeviceCommand = new Command(SetToUnLockDevice);
+            HalfLockDeviceCommand = new Command(SetToHalfLockDevice);
+
         }
 
-        private void GetStatus(object obj)
+        private void GetStatus()
         {
             Task.Delay(100);
             Services.SMS.Commands.Status();
-           UserDialogs.Instance.Toast("درخواست وضعیت با موفقیت ارصال شد.");
+            UserDialogs.Instance.Toast("درخواست وضعیت با موفقیت ارصال شد.");
+        }
+
+        private void SetToLockDevice()
+        {
+            Task.Delay(100);
+            Services.SMS.Commands.Close();
+            UserDialogs.Instance.Toast("درخواست قفل کردن دستگاه با موفقیت ارسال شد.");
+        }
+
+        private void SetToHalfLockDevice()
+        {
+            Task.Delay(100);
+            Services.SMS.Commands.PartialOpen();
+            UserDialogs.Instance.Toast("درخواست نیمه باز کردن دستگاه با موفقیت ارسال شد.");
+        }
+
+        private void SetToUnLockDevice()
+        {
+            Task.Delay(100);
+            Services.SMS.Commands.Open();
+            UserDialogs.Instance.Toast("درخواست باز کردن دستگاه با موفقیت ارسال شد.");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
         public ICommand StatusDeviceCommand { get; }
+        public ICommand LockDeviceCommand { get;}
+        public ICommand UnLockDeviceCommand { get;}
+        public ICommand HalfLockDeviceCommand { get;}
     }
 }
